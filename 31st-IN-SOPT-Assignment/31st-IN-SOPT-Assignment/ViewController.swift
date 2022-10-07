fedb2aff3cee296f056542f0033b7b32966cdfc5
cchanmi <gkfn980@hs.ac.kr>
2022-10-07 15:03:59 +0900

    [#1] 프로젝트 생성

---
+++ 31st-IN-SOPT-Assignment/31st-IN-SOPT-Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  31st-IN-SOPT-Assignment
+//
+//  Created by 황찬미 on 2022/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
