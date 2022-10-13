74693a9fdd403f620b690115eeb189a132f258e3
cchanmi <gkfn980@hs.ac.kr>
2022-10-13 18:29:01 +0900

    [#7] profile background color 추가

--- 31st-IN-SOPT-Assignment/31st-IN-SOPT-Assignment/Global/Extension/UIColor+.swift
+++ 31st-IN-SOPT-Assignment/31st-IN-SOPT-Assignment/Global/Extension/UIColor+.swift
@@ -11,4 +11,8 @@ extension UIColor {
   @nonobjc class var kakaoYellow: UIColor {
       return UIColor(red: 251.0 / 255.0, green: 229.0 / 255.0, blue: 76.0 / 255.0, alpha: 1.0)
   }
+    
+  @nonobjc class var backgroundGray: UIColor {
+      return UIColor(red: 135.0 / 255.0, green: 145.0 / 255.0, blue: 152.0 / 255.0, alpha: 1.0)
+    }
 }
