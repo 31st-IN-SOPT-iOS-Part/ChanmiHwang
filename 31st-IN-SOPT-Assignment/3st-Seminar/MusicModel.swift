45dc87e9a79498850452ce1dfa5690cd0c0a22b5
cchanmi <gkfn980@hs.ac.kr>
2022-11-19 13:16:02 +0900

    [#12] 세미나 파일 추가

---
+++ 31st-IN-SOPT-Assignment/3st-Seminar/MusicModel.swift
@@ -0,0 +1,14 @@
+//
+//  MusicModel.swift
+//  31st-IN-SOPT-Assignment
+//
+//  Created by 황찬미 on 2022/11/19.
+//
+
+import Foundation
+
+struct MusicModel {
+    let albumImage: String
+    let title: String
+    let singer: String
+}
